35579636e58498f8ef8b835ad5590d32f69820ca
Stuart Kent <skent.dev@gmail.com>
2016-02-26 11:09:25 -0500

    Demonstrate calculations for changing aspect ratio of child views

--- app/src/main/java/com/example/insetchildviewgroup/MainActivity.java
+++ app/src/main/java/com/example/insetchildviewgroup/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.insetchildviewgroup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.stkent.insetchildviewgroup.InsetChildViewGroup;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private InsetChildViewGroup insetChildViewGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insetChildViewGroup = (InsetChildViewGroup) findViewById(R.id.inset_child_view_group);
+
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seek_bar);
+        seekBar.setOnSeekBarChangeListener(this);
+        seekBar.setProgress(9);
+    }
+
+    @Override
+    public void onProgressChanged(final SeekBar seekBar, final int progress, final boolean fromUser) {
+        insetChildViewGroup.setChildViewAspectRatio(computeAspectRatioFromProgress(progress));
+    }
+
+    @Override
+    public void onStartTrackingTouch(final SeekBar seekBar) {
+        // This method intentionally left blank
+    }
+
+    @Override
+    public void onStopTrackingTouch(final SeekBar seekBar) {
+        // This method intentionally left blank
+    }
+
+    private float computeAspectRatioFromProgress(final int progress) {
+        return (progress + 1) / 10f;
     }
 
 }
--- insetchildviewgroup/src/main/java/com/github/stkent/insetchildviewgroup/InsetChildViewGroup.java
+++ insetchildviewgroup/src/main/java/com/github/stkent/insetchildviewgroup/InsetChildViewGroup.java
@@ -1,26 +1,104 @@
 package com.github.stkent.insetchildviewgroup;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
+
+import static android.graphics.Paint.ANTI_ALIAS_FLAG;
+import static java.lang.Math.ceil;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
 public class InsetChildViewGroup extends ViewGroup {
 
+    public static final Paint CIRCLE_PAINT = new Paint(ANTI_ALIAS_FLAG);
+
+    static {
+        CIRCLE_PAINT.setColor(Color.BLUE);
+        CIRCLE_PAINT.setStrokeWidth(40);
+        CIRCLE_PAINT.setStyle(Paint.Style.STROKE);
+    }
+
+    private final Rect childViewRect = new Rect();
+
+    /**
+     * Aspect ratio of child view, such that
+     *
+     *     w = LAMBDA h
+     *
+     * where w is the width of the child view, and h is the height of the child view.
+     */
+    private float lambda = 0.2f;
+
     public InsetChildViewGroup(final Context context) {
-        super(context);
+        this(context, null);
     }
 
     public InsetChildViewGroup(final Context context, final AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public InsetChildViewGroup(final Context context, final AttributeSet attrs, final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setWillNotDraw(false);
+    }
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        //noinspection SuspiciousNameCombination
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+
+        final double circleInternalRadius = getMeasuredWidth() / 2 - CIRCLE_PAINT.getStrokeWidth();
+        final double childViewHeight = 2 * circleInternalRadius / sqrt(1 + pow(lambda, 2));
+        final double childViewWidth = lambda * childViewHeight;
+
+        childViewRect.set(
+                (int) ceil((getMeasuredWidth() - childViewWidth) / 2),
+                (int) floor((getMeasuredHeight() - childViewHeight) / 2),
+                (int) ceil((getMeasuredWidth() + childViewWidth) / 2),
+                (int) floor((getMeasuredHeight() + childViewHeight) / 2));
+
+        final int childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(
+                childViewRect.height(),
+                MeasureSpec.EXACTLY);
+
+        final int childWidthMeasureSpec = MeasureSpec.makeMeasureSpec(
+                childViewRect.width(),
+                MeasureSpec.EXACTLY);
+
+        for (int childIndex = 0; childIndex < getChildCount(); childIndex++) {
+            getChildAt(childIndex).measure(childWidthMeasureSpec, childHeightMeasureSpec);
+        }
     }
 
     @Override
     protected void onLayout(final boolean changed, final int l, final int t, final int r, final int b) {
+        for (int childIndex = 0; childIndex < getChildCount(); childIndex++) {
+            final View child = getChildAt(childIndex);
+            child.layout(
+                    childViewRect.left,
+                    childViewRect.top,
+                    childViewRect.right,
+                    childViewRect.bottom);
+        }
+    }
 
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawCircle((int) (getMeasuredWidth() / 2.0), (int) (getMeasuredHeight() / 2.0), (getWidth() - CIRCLE_PAINT.getStrokeWidth()) / 2, CIRCLE_PAINT);
+    }
+
+    public void setChildViewAspectRatio(final float lambda) {
+        this.lambda = lambda;
+        requestLayout();
     }
 
 }
